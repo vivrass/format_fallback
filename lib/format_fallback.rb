3a791cb321a44d0ca208a2e2b7ddb4afb5553023
Martin Provencher <mprovencher86@gmail.com>
2012-03-30 17:56:31 -0400

    Seems to be easier like that

--- lib/format_fallback.rb
+++ lib/format_fallback.rb
@@ -1,6 +1,7 @@
 require "format_fallback/version"
 
 module ActionView
+=begin
   class PathSet
 
     def find_with_default_template(*args)
@@ -9,29 +10,21 @@ module ActionView
         # Layouts have their own way of managing fallback, better leave them alone
         find_without_default_template(*args)
       else
-        options = args[3]
-        options[:formats] << :html unless options[:formats].include?(:html)
+        details = args[3]
+        details[:formats] << :html unless details[:formats].include?(:html)
         find_without_default_template(*args)
       end
     end
     alias_method_chain :find, :default_template
 
   end
-  
-=begin
-  # This doesn't seem to be useful. Keep it until we can test in production with caching
-  class Resolver
-    
-    def cached(key, prefix, name, partial)
-      return yield unless key && caching?
-      cache_content = yield
-      if cache_content.empty?
-        []
-      else
-        @cached[key][prefix][name][partial] ||= cache_content
-      end
-    end
-    
-  end
 =end
+  
+  class PathResolver
+    def find_templates_with_default(name, prefix, partial, details)
+      details[:formats] << :html unless details[:formats].include?(:html)
+      find_templates_without_default(name, prefix, partial, details)
+    end
+    alias_method_chain :find_templates, :default
+  end
 end
